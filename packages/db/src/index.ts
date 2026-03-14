31c351bd5be684a50882660e12292c3acc13cd8f
Tuyisenge2 <tuyisengetito3@gmail.com>
2026-03-14 14:19:36 +0200

    adding chekc for production dev 7

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
-if (process.env.NODE_ENV !== 'production') {
-	dotenv.config({
-		path: "../../apps/server/.env",
-	});
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({
+    path: "../../apps/server/.env",
+  });
 }
 
 import { neon, neonConfig } from "@neondatabase/serverless";
@@ -17,5 +17,9 @@ neonConfig.webSocketConstructor = ws;
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
 
-const sql = neon(process.env.DATABASE_URL || "");
+console.log('77777777777777777777777777777777777777777777777777',process.env.DATABASE_URL)
+const sql = neon(
+  process.env.DATABASE_URL ||
+    "postgresql://neondb_owner:npg_WtUqCazK10Do@ep-dawn-sun-ad6ds95v-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+);
 export const db = drizzle(sql, { schema });
